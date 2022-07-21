696a61007b7eb34adadd530f27ddf3ba3dd22ace
unknown <teyeetukudo@gmail.com>
2022-07-21 14:53:20 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+
+    colors: {
+      'blue-dark': '#0D2549',
+      'blue-light': '#0477BF',
+    },
+
+    extend: {
+    },
   },
-  plugins: [],
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
