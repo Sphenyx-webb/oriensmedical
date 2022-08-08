caeb2cee2fa51e1eb1e18df8561131a8e4526ea2
unknown <teyeetukudo@gmail.com>
2022-08-08 01:35:49 -0700

    updated version

---
+++ dist/static/js/video-control.js
@@ -0,0 +1,10 @@
+var introVid = document.getElementById("intro-vid");
+var vidCover = document.getElementById("vid-cover");
+var playBtn = document.querySelector(".fa-play");
+
+playBtn.addEventListener("click", () => {
+    vidCover.classList.remove("z-10")
+    introVid.play()
+})
+
+
