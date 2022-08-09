c443c558e2af5c358fc7131dd6fb66852c79c4ea
Nate-Soul <nanithsukpong@gmail.com>
2022-08-09 11:32:09 +0100

    --minor fix 2

--- dist/static/js/video-control.js
+++ dist/static/js/video-control.js
@@ -1,11 +1,23 @@
-var vid = document.getElementById("vid");
-var vidCover = document.getElementById("vid-cover");
-var playBtn = document.querySelector(".fa-play");
+const homeVid = document.querySelector("#homeVideo");
+const homeVidCover = document.querySelector("#homeVideoCover");
+const playBtn = document.querySelector(".fa-play");
 
-playBtn.addEventListener("click", () => {
-    vidCover.classList.remove("z-10")
-    vid.play()
-    vid.setAttribute("controls", true);
-})
+//home video control script
+if(homeVid && homeVidCover && playBtn){
+    playBtn.addEventListener("click", () => {
+        homeVidCover.classList.remove("z-10")
+        homeVid.play()
+        homeVid.setAttribute("controls", true);
+    });
+}
 
-
+//about video control script
+// const aboutVid = document.querySelector("#aboutVideo");
+// const aboutVidCover = document.querySelector("#aboutVideoCover");
+// if(aboutVid && aboutVidCover && playBtn){
+//     playBtn.addEventListener("click", () => {
+//         aboutVidCover.classList.remove("z-10")
+//         document.querySelector("button.play").classList.remove("state-paused");
+//         document.querySelector("button.play").classList += "state-playing";
+//     });
+// }
